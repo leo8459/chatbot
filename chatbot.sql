d2335c7fd0337ef778bf288e94d7e82c0ca0c347
Leo <joseaguilar987654321@gmail.com>
2025-03-10 15:12:53 -0400

    Update chatbot.sql

--- chatbot.sql
+++ chatbot.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.4.28-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.4.0.6659
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.3.0.6589
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -15,41 +15,48 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para chatbot
-CREATE DATABASE IF NOT EXISTS `chatbot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `chatbot`;
+-- Dumping database structure for chatbot2
+CREATE DATABASE IF NOT EXISTS `chatbot2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `chatbot2`;
 
--- Volcando estructura para tabla chatbot.chatbot
-CREATE TABLE IF NOT EXISTS `chatbot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- Dumping structure for table chatbot2.chatbot2
+CREATE TABLE IF NOT EXISTS `chatbot2` (
+  `id` int(11) NOT NULL,
   `queries` varchar(300) NOT NULL,
-  `replies` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `replies` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla chatbot.chatbot: ~20 rows (aproximadamente)
-DELETE FROM `chatbot`;
-INSERT INTO `chatbot` (`id`, `queries`, `replies`) VALUES
+-- Dumping data for table chatbot2.chatbot2: ~29 rows (approximately)
+INSERT INTO `chatbot2` (`id`, `queries`, `replies`) VALUES
 	(1, '1', '10. EMS <br>\n11. Envíos Certificados<br>\n12. Envíos Ordinarios <br>\n13. Encomienda<br>\n\n'),
-	(2, '2', 'Vale te pregunto, donde te encuentras ahora, ¿hay más dispositivos conectados a tu red local?'),
-	(3, '3', 'Tienes algún ordenador conectado por cable'),
-	(4, '4', 'Si por favor y me confirmas si no tienes acceso a Internet, en tu ordenador cuando encienda'),
-	(5, '5', 'Reinicia tu modem, espera 10 minutos y reintenta, me confirmas'),
-	(6, '6', 'Excelente me alegro mucho, gracias por comunicarte con Soporte Técnico ChatBot ConfiguroWeb'),
+	(2, '2', '20 Nacional <br>\n21 Internacional'),
+	(3, '3', 'Los países a los que no llegamos <br>\nUkrania <br>\nRusia<br>\nCuba <br>\n<a href="https://www.correos.gob.bo/" style="color: blue;">Visitar Países en mapa Interactivo</a>'),
+	(4, '4', 'La mercancia prohibida de mandar es: <br>\n- <br>\n- Sustancia controladas<br>\n-Hojas de Coca <br>\n- Animales<br>\n- Pastillas y Medicamentos<br>\n- Armamento <br>\n- Objetos de valor<br>\n-  Maquinas Electricas<br>\n- Baterias y pilas<br>\n- Sustancias Quimicas<br>\n- Gases Y Liquidos Comprimidos<br>\n- Explosivos<br>\n- Fosforos y Encendedores<br>    \nPara Informacion mas detallada Haga click en el siguiente enlace\n<br><a href=https://drive.google.com/file/d/1UmlDz07vL6HeIbMiaCQsDAaylR_ljf72/view?usp=sharing style="color: white;">Click Aqui</a>\n'),
+	(5, '5', 'Para Rotular tu envio necesitaras tener estos 3 aspectos importantes: <br>\n- Remitente<br>\n- Destinatario<br>\n- Sello postal<br>\npara una información mas detallada haga click en link que se tiene abajo <br>\n    <a href=https://drive.google.com/file/d/1xtaytu_JNf3QsKGmQZz6CJePVVCXYJ9W/view?usp=sharing style="color: blue;"> click aqui</a>\n'),
+	(6, '6', 'Para calcular tu peso volumétrico y tener información de como haga click en el siguiente enlace: <br>\n<a href=https://drive.google.com/file/d/1QIh16pqrxI4_HY8zswQJup6mHhuQ2CEU/view?usp=sharing style="color: blue;"> click aqui</a>'),
 	(7, '7 ', 'Horarios de Atencion:<br>\r\nLun - Vie 08:00 a 18:00 Hrs <br>\r\nSabados: 09:00 - 13:00'),
-	(8, '0', 'Hola, Bienvenido a la Agencia Boliviana de Correos ¿cómo puedo ayudarte?\r\n                         <br>(selecciona la opcion que deseas escribe el numero que deseas)<br>\r\n                        1. Tipos de servicios que ofrecemos<br>\r\n                        2. A donde va a enviar?<br>\r\n                        3. A donde no llegamos<br>\r\n                        4. Mercaderias Prohibidas<br>\r\n                        5. Como Rotular Un Envio Tradicional<br>\r\n                        6. Peso Volumetrico del paquete<br>\r\n                        7. Horarios de Atencion<br>\r\n                        8. Precios de Casillas<br>\r\n  9. Volver Pagina Principal<br>\r\n0. Volver al Menu Principal'),
-	(9, '10', 'Express Mail Service (EMS) <BR>\r\nEs un servicio de envio de mensajeria y paqueteria urgente. Llega a las ciudades capitales de los 9 departamentos de Bolivia, el tiempo de entrega difiere entre las 24 y 72 horas, siendo la distancia entre la cuidad de Origen y la ciudad del factor determinante para el calculo de tiempo de llegada<br>\r\nA nivel Internacional tiene alcance a todos los paises miembros de la UNION POSTAL UNIVERSAL haciendo de este servicio de clase mundial.<br>\r\n14. Entrega Nacional.<br>\r\n15. Entrega Internacional.\r\n '),
-	(10, '8', 'Precios de Casillas:<br>\r\nPequeñas:<br>\r\nMensual: 35Bs<br>\r\nSemestral: 126Bs<br>\r\nAnual: 210Bs<br>\r\nMediana:<br>\r\nMensual: 35Bs<br>\r\nSemestral: 175Bs<br>\r\nAnual: 294Bs<br>\r\nGavetas:<br>\r\nAnual: 420Bs<br>\r\nCajon: <br>\r\nAnual: 504Bs<br>\r\n\r\n'),
-	(11, '9', '    <a href="https://www.correos.gob.bo/" style="color: white;">Volver pagina principal click aqui</a>\';\r\n'),
-	(12, '14', 'Para mas Información Sobre EMS Nacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1xzA7GRtA_Mk_4tfBsAlHFT6V2VlR2Pdc/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'\r\n\r\n '),
+	(8, '0', ' Hola, Bienvenido a la Agencia Boliviana de Correos\n                        <br>(SELECCIONA EL NUMERO QUE TENGA LA OPCION QUE DESEAS) <BR>\n                        1. Tipos de servicios que ofrecemos<br>\n                        2. Precios de envio, ¿a donde quieres enviar? <br>\n                        3. A donde no llegamos<br>\n                        4. Mercaderias Prohibidas<br>\n                        5. Como Rotular Un Envio Tradicional<br>\n                        6. Peso Volumetrico del paquete<br>\n                        7. Horarios de Atencion<br>\n                        8. Precios de Casillas<br>\n                        9. Volver Pagina Web<br>\n                        0. Si tu pregunta no se encuentra comunicate con nosotros al numero 76457323<br>\n                        Todo servicio que se brinda en este chat es solo de consultas<br>'),
+	(9, '10', 'Express Mail Service (EMS) <BR>\nEs un servicio de envio de mensajeria y paqueteria urgente. Llega a las ciudades capitales de los 9 departamentos de Bolivia, el tiempo de entrega difiere entre las 24 y 72 horas, siendo la distancia entre la cuidad de Origen y la ciudad del factor determinante para el calculo de tiempo de llegada<br>\nA nivel Internacional tiene alcance a todos los paises miembros de la UNION POSTAL UNIVERSAL haciendo de este servicio de clase mundial.<br>\n14. Entrega Nacional.<br>\n15. Entrega Internacional.\n '),
+	(10, '8', 'Precios de Casillas:<br>\nPequeñas:<br>\nMensual: 35Bs<br>\nSemestral: 126Bs<br>\nAnual: 210Bs<br>\nMediana:<br>\nMensual: 35Bs<br>\nSemestral: 175Bs<br>\nAnual: 294Bs<br>\nGavetas:<br>\nAnual: 420Bs<br>\nCajon: <br>\nAnual: 504Bs<br>\n\n'),
+	(11, '9', '    <a href="https://www.correos.gob.bo/" style="color: white;">Volver pagina principal click aqui</a>\';\n'),
 	(13, '15', 'Para mas Información Sobre EMS Internacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1G3VnX8NvsyyPwQAc_QmXBMPKT8qCvjRQ/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'\r\n'),
 	(14, '11', 'Todas las cartas sobre manila dependiendo del tamaño para su verificación tiene un coste   de:<br>\r\nNacional:2Bs<br>\r\nInternacional:8Bs<br>\r\n\r\n'),
 	(15, '12', 'El servicio ordinario se refiere a tarjeras postales que no llevan rastreo, los precios son:<br>\r\n16. Nacional<br>\r\n17. Internacional'),
 	(16, '16', 'Para mas Información Sobre Servicio Ordinario Nacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1ZuQuNxSjvSoxS4IdYXl9dDgapfEu6WTV/view?usp=sharing" style="color: white;">Haz click Aqui</a>\''),
 	(17, '17', 'Para mas Información Sobre Servicio Ordinario Internacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1_ZHNIN8lU4jDABnSLrfTNENOofbbdX9W/view?usp=sharing"color: white;">Haz click Aqui</a>\''),
 	(18, '13', '18. Nacional<br>\r\n19. Internacional'),
-	(19, '18', 'Para mas Información Sobre Encomiendas Nacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href=https://drive.google.com/file/d/1XJ4JajeEskNW3FovXrcg0w_gQA6fwgk5/view?usp=sharing" style="color: white;">Haz click Aqui</a>\''),
-	(20, '19', 'Para mas Información Sobre Encomiendas Internacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1Ls84MUSnlIPCH31o1YYgYxZPDme0hphe/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'');
+	(19, '18', 'Para mas Información Sobre Encomiendas Nacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\n    <a href=https://drive.google.com/file/d/1XJ4JajeEskNW3FovXrcg0w_gQA6fwgk5/view?usp=sharing" style="color: white;">Haz click Aqui</a>\''),
+	(20, '19', 'Para mas Información Sobre Encomiendas Internacional descarga o dirígete al siguiente link donde se proporcionara los precios a detalle<br>\r\n    <a href="https://drive.google.com/file/d/1Ls84MUSnlIPCH31o1YYgYxZPDme0hphe/view?usp=sharing" style="color: white;">Haz click Aqui</a>\''),
+	(21, '20', 'Que tipo de servicio quiere consultar para su envio nacional <br>\n22. EMS <BR>\n23. Mi Encomienda <BR>\n24. Servicios Especiales<BR>\n'),
+	(22, '22', 'Para la informacion de precios a envios nacional con el servicio ems haga click aqui<br>todos los precios estan descritos en el documento<br>\n <a href=https://drive.google.com/file/d/1V2l4kQaeZXFwzD97SXEcc_LqhY-CQIho/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'\n'),
+	(23, '23', 'Para la informacion de precios a envios nacional de mi encomienda haga click aqui (toda informacion proporcionada es unicamente informacion)<br>\n  <a href=https://drive.google.com/file/d/1XJ4JajeEskNW3FovXrcg0w_gQA6fwgk5/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'\n'),
+	(24, '24', 'Para la informacion de precios a envios nacional servicios especiales haga click aqui (toda informacion proporcionada es unicamente informacion)<br>\r\n  <a href=https://drive.google.com/file/d/1WZ9uLqqYmvJMctYsuj8wY6avveZRNzt3/view?usp=sharing color: white;">Haz click Aqui</a>\'\r\n'),
+	(25, '25', 'Para la informacion de precios a envios internacional con el servicio ems haga click aqui<br>todos los precios estan descritos en el documento<br>\n <a href=https://drive.google.com/file/d/1G3VnX8NvsyyPwQAc_QmXBMPKT8qCvjRQ/view?usp=sharing style="color: white;">Haz click Aqui</a>\'\n'),
+	(26, '26', 'Para la informacion de precios a envios internacional de mi encomienda haga click aqui (toda informacion proporcionada es unicamente informacion)<br>\n  <a href=https://drive.google.com/file/d/1Ls84MUSnlIPCH31o1YYgYxZPDme0hphe/view?usp=sharing style="color: white;">Haz click Aqui</a>\'\n'),
+	(27, '27', 'Para la informacion de precios a envios nacional servicios especiales haga click aqui (toda informacion proporcionada es unicamente informacion)<br>\n  <a href=https://drive.google.com/file/d/1WZ9uLqqYmvJMctYsuj8wY6avveZRNzt3/view?usp=sharing color: white;">Haz click Aqui</a>\'\n'),
+	(28, '21', 'Que tipo de servicio quiere consultar para su envió Internacional<br>\r\n25. EMS <BR>\r\n26. Mi Encomienda <BR>\r\n27. Servicios Especiales<BR>\r\n'),
+	(30, '0', '    <a href="https://www.correos.gob.bo/" style="color: white;">Volver pagina principal click aqui</a>\';\r\n'),
+	(33, '14', 'Para la informacion de precios a envios nacional con el servicio ems haga click aqui<br>todos los precios estan descritos en el documento<br>\r\n <a href=https://drive.google.com/file/d/1V2l4kQaeZXFwzD97SXEcc_LqhY-CQIho/view?usp=sharing" style="color: white;">Haz click Aqui</a>\'\r\n');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
